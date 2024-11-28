74d12fdc81dff2218ebce17f9606450ca228f6f2
Julian Pineda <julian.pineda21@gmail.com>
2024-11-28 11:12:31 -0500

    add user to event table in create

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ApplicationRecord
-  validates :name, :date, :description, :capacity, :category, :status, :place, presence: true
+  belongs_to :user
 
   enum status: { active: 0, deleted: 1, archived: 1 }
   enum category: { conference: 0, workshop: 1, webinar: 2, concert: 3, festival: 4, course: 5, networking: 6 }
+
+  validates :name, :date, :description, :capacity, :category, :status, :place, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_27_133727) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_28_160341) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_27_133727) do
     t.integer "status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
