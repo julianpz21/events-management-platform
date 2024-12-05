f818c66e2e132860372525a5dd12a5b17d9a866a
Julian Pineda <julian.pineda21@gmail.com>
2024-12-05 14:50:42 -0500

    add improvements

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,19 +1,7 @@
 class Api::BaseController < ActionController::API
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  include Authorizationable
 
   rescue_from CanCan::AccessDenied do |exception|
-    exception.default_message = "You are not authorized to perform this task"
-    respond_to do |format|
-      format.json { head :forbidden }
-      format.html { redirect_back(fallback_location: root_path, alert: exception.message) }
-    end
-  end
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :role ])
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
+    render json: { error: "Access Denied: #{exception.message}" }, status: :forbidden
   end
 end
